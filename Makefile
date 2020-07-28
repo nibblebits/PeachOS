46b01b69d2e09b39438933cba5f61fb35b4a7d4e
Nibblebits <dansblackberry2@hotmail.co.uk>
2020-07-28 11:22:30 +0100

    Entering protected mode

---
+++ Makefile
@@ -0,0 +1,4 @@
+all:
+	nasm -f bin ./boot.asm -o ./boot.bin
+	dd if=./message.txt >> ./boot.bin
+	dd if=/dev/zero bs=512 count=1 >> ./boot.bin
--- boot.asm
+++ boot.asm
@@ -1,28 +1,77 @@
 ORG 0x7c00
 BITS 16
 
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
+_start:
+    jmp short start
+    nop
+
+ times 33 db 0
+ 
 start:
-    mov si, message
-    call print
+    jmp 0:step2
+
+step2:
+    cli ; Clear Interrupts
+    mov ax, 0x00
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7c00
+    sti ; Enables Interrupts
+
+.load_protected:
+    cli
+    lgdt[gdt_descriptor]
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:load32
+
+; GDT
+gdt_start:
+gdt_null:
+    dd 0x0
+    dd 0x0
+
+; offset 0x8
+gdt_code:     ; CS SHOULD POINT TO THIS
+    dw 0xffff ; Segment limit first 0-15 bits
+    dw 0      ; Base first 0-15 bits
+    db 0      ; Base 16-23 bits
+    db 0x9a   ; Access byte
+    db 11001111b ; High 4 bit flags and the low 4 bit flags
+    db 0        ; Base 24-31 bits
+
+; offset 0x10
+gdt_data:      ; DS, SS, ES, FS, GS
+    dw 0xffff ; Segment limit first 0-15 bits
+    dw 0      ; Base first 0-15 bits
+    db 0      ; Base 16-23 bits
+    db 0x92   ; Access byte
+    db 11001111b ; High 4 bit flags and the low 4 bit flags
+    db 0        ; Base 24-31 bits
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start-1
+    dd gdt_start
+ 
+[BITS 32]
+load32:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov ebp, 0x00200000
+    mov esp, ebp
     jmp $
 
-print:
-    mov bx, 0
-.loop:
-    lodsb
-    cmp al, 0
-    je .done
-    call print_char
-    jmp .loop
-.done:
-    ret
-
-print_char:
-    mov ah, 0eh
-    int 0x10
-    ret
-
-message: db 'Hello World!', 0
 
 times 510-($ - $$) db 0
 dw 0xAA55
