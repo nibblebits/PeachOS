9f57e1cfbf325843843e7126f2aa1c552135e50e
Nibblebits <dansblackberry2@hotmail.co.uk>
2020-12-09 11:30:13 +0000

    Lecture 92 - Revising our stream reader

--- src/disk/streamer.c
+++ src/disk/streamer.c
@@ -1,6 +1,8 @@
 #include "streamer.h"
 #include "memory/heap/kheap.h"
 #include "config.h"
+
+#include <stdbool.h>
 struct disk_stream* diskstreamer_new(int disk_id)
 {
     struct disk* disk = disk_get(disk_id);
@@ -25,7 +27,14 @@ int diskstreamer_read(struct disk_stream* stream, void* out, int total)
 {
     int sector = stream->pos / PEACHOS_SECTOR_SIZE;
     int offset = stream->pos % PEACHOS_SECTOR_SIZE;
+    int total_to_read = total;
+    bool overflow = (offset+total_to_read) >= PEACHOS_SECTOR_SIZE;
     char buf[PEACHOS_SECTOR_SIZE];
+
+    if (overflow)
+    {
+        total_to_read -= (offset+total_to_read) - PEACHOS_SECTOR_SIZE;
+    }
 
     int res = disk_read_block(stream->disk, sector, 1, buf);
     if (res < 0)
@@ -33,7 +42,7 @@ int diskstreamer_read(struct disk_stream* stream, void* out, int total)
         goto out;
     }
 
-    int total_to_read = total > PEACHOS_SECTOR_SIZE ? PEACHOS_SECTOR_SIZE : total;
+   
     for (int i = 0; i < total_to_read; i++)
     {
         *(char*)out++ = buf[offset+i];
@@ -41,9 +50,9 @@ int diskstreamer_read(struct disk_stream* stream, void* out, int total)
 
     // Adjust the stream
     stream->pos += total_to_read;
-    if (total > PEACHOS_SECTOR_SIZE)
+    if (overflow)
     {
-        res = diskstreamer_read(stream, out, total-PEACHOS_SECTOR_SIZE);
+        res = diskstreamer_read(stream, out, total-total_to_read);
     }
 out:
     return res;
