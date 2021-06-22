cf3b3f45cede6c79d785609f68b13039a442099e
Daniel McCarthy/AKA NibbleBits <dansblackberry2@hotmail.co.uk>
2021-06-22 13:48:29 +0100

    Lecture 124 - Changing our fat16_new_fat_item_for_directory_item function

--- src/fs/fat/fat16.c
+++ src/fs/fat/fat16.c
@@ -560,6 +560,7 @@ struct fat_item *fat16_new_fat_item_for_directory_item(struct disk *disk, struct
     {
         f_item->directory = fat16_load_fat_directory(disk, item);
         f_item->type = FAT_ITEM_TYPE_DIRECTORY;
+        return f_item;
     }
 
     f_item->type = FAT_ITEM_TYPE_FILE;
